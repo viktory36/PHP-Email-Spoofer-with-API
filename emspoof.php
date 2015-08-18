2145840f8ff2ce8bd74578028a08c5efff6152f6
vedant <vedanttheepic@gmail.com>
2015-08-18 18:23:43 +0530

    fixed bugs and added a spammer thingy

--- emspoof.php
+++ emspoof.php
@@ -7,7 +7,6 @@
 	margin: 0;
 	padding: 0;
 }
-
 body {
 	background-color: #fff;
 	color: #333333;
@@ -15,14 +14,12 @@ body {
 	margin-bottom: 30px;
         text-align: center;
 }
-
 h1, h2, h3, h4 {
 	font: Garamond;
 	font-size: 22px;
 	letter-spacing: -1px;
 	text-transform: uppercase;
 }
-
 h4 {
 	font-size: 12px;
 }
@@ -31,7 +28,6 @@ h4 {
 <body>
 <centre>
 <?php
-
 //declarin stoof
   if (isset($_REQUEST['from']))  {
   
@@ -41,15 +37,14 @@ h4 {
   $fromname= $_REQUEST['fromname'];
   $subject = $_REQUEST['subject'];
   $message= $_REQUEST['message'];
+  $t2s= $_REQUEST['t2s'];
   $header= "From: $fromname<$from>";
-
   
   //send email
+ for ($i=1; $i<=$t2s; $i++){
   mail($to, "$subject", $message, $header);
-  
-echo "Email sent to $to using email $from and name $fromname";
-echo "<br /><a href='http://google.com'>Click here to send another email.</a><br />";
-
+  }
+echo htmlentities("Email sent $t2s times to $to using email $from and name $fromname");
  }
   
   else  {
@@ -62,13 +57,12 @@ echo "<br /><a href='http://google.com'>Click here to send another email.</a><br
   Subject: <input name="subject" type="text" /><br />
   Message:<br />
   <textarea name="message" rows="15" cols="40"></textarea><br />
+  Times to send: <input name="t2s" type="text" /><br />
   <input type="submit" value="Submit" />
   </form>
 
-<!--<h3>viktory</h3>-->
 </centre>
 </body>
 <?
 }
-echo "<br><a href='http://google.com'>API available here.</a>";
 ?>
